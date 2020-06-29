4203e358e83eb22ff49b5d88f2459332bfa07520
Boriss Arhipov <bori.7166455@gmail.com>
2020-06-29 17:40:46 +0300

    Making basic setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,83 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
+module.exports = (env = {}) => {
 
-module.exports = {
-  mode: 'development',
-  entry: './src/app.ts',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  }
+    return {
+        mode: 'development',
+        entry: './src/components/app/app.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,            
+                    use: [
+                        {
+                            loader: 'babel-loader'
+                        }
+                    ],
+                    exclude: '/node_modules/'
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        { loader: MiniCssExtractPlugin.loader },
+                        { loader: 'css-loader'}
+                    ]
+                },
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.(s[ca]ss)$/,
+                    use: [
+                        { loader: MiniCssExtractPlugin.loader },
+                        { loader: 'css-loader'},
+                        { loader: 'sass-loader'}
+                    ]
+                },
+                {
+                    test: /\.(png|jpg|jpeg|gif|ico)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                outputPath: 'images',
+                                name: '[name]-[sha1:hash:7].[ext]'
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.(ttf|otf|eot|woff|woff2)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                outputPath: 'fonts',
+                                name: '[name].[ext]'
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: 'index.html'
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'main-[hash:8].css'
+            })
+        ],
+
+        devServer: {
+            open: true
+        }
+    }
 };
