b03bf9ed0e255b08a32365203406a063c3d660ef
Boriss Arhipov <bori.7166455@gmail.com>
2020-06-30 17:22:06 +0300

    Making a merge for player and arena matrix. Adding collision detecting fuction

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -10,6 +10,53 @@ class Tetris {
         this.dropInterval = 1000;
         this.lastTime = 0;
         this.update();
+        this.keyControls();
+    }
+    collide(arena, player) {
+        const [m, o] = [player.matrix, player.pos];
+        for (let y = 0; y < m.length; ++y) {
+            for (let x = 0; x < m[y].length; ++x) {
+                if (m[y][x] !== 0 &&
+                    (arena.matrix[y + o.y] && arena.matrix[y + o.y][x + o.x]) !== 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    merge(arena, player) {
+        player.matrix.forEach((row, y) => {
+            row.forEach((value, x) => {
+                if (value !== 0) {
+                    arena.matrix[y + player.pos.y][x + player.pos.x] = value;
+                }
+            });
+        });
+    }
+    movePiece(dir) {
+        player.pos.x += dir;
+        if (this.collide(arena, player)) {
+            player.pos.x -= dir;
+        }
+    }
+    keyControls() {
+        document.addEventListener('keydown', event => {
+            if (event.keyCode === 65) {
+                this.movePiece(-1);
+            }
+            if (event.keyCode === 68) {
+                this.movePiece(1);
+            }
+            if (event.keyCode === 83) {
+                player.pos.y++;
+                if (this.collide(arena, player)) {
+                    player.pos.y--;
+                    this.merge(arena, player);
+                    player.pos.y = 0;
+                }
+                this.dropCounter = 0;
+            }
+        });
     }
     drawMatrix(matrix, offset) {
         matrix.forEach((row, y) => {
@@ -24,6 +71,7 @@ class Tetris {
     draw() {
         this.context.fillStyle = '#000';
         this.context.fillRect(0, 0, this.tetrisCanvas.width, this.tetrisCanvas.height);
+        this.drawMatrix(arena.matrix, { x: 0, y: 0 });
         this.drawMatrix(player.matrix, player.pos);
     }
     update(time = 0) {
@@ -32,6 +80,11 @@ class Tetris {
         this.dropCounter += deltaTime;
         if (this.dropCounter > this.dropInterval) {
             player.pos.y++;
+            if (this.collide(arena, player)) {
+                player.pos.y--;
+                this.merge(arena, player);
+                player.pos.y = 0;
+            }
             this.dropCounter = 0;
         }
         this.draw();
@@ -56,6 +109,19 @@ class Player {
         this.pos = { x: 5, y: 5 };
     }
 }
+class Arena {
+    constructor() {
+        this.matrix = this.createMatrix(12, 20);
+    }
+    createMatrix(w, h) {
+        const matrix = [];
+        while (h--) {
+            matrix.push(new Array(w).fill(0));
+        }
+        return matrix;
+    }
+}
 const tetrisPieces = new TetrisPieces;
 const player = new Player;
+const arena = new Arena;
 new Tetris;
--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -2,11 +2,11 @@ import './app.css';
 
 // Tetris class
 class Tetris {
-    tetrisCanvas: HTMLCanvasElement;
-    context: CanvasRenderingContext2D;
-    lastTime: number;
-    dropCounter: number;
-    dropInterval: number;
+    private tetrisCanvas: HTMLCanvasElement;
+    private context: CanvasRenderingContext2D;
+    private lastTime: number;
+    private dropCounter: number;
+    private dropInterval: number;
 
     constructor() {
         this.tetrisCanvas = document.getElementById('tetris') as HTMLCanvasElement;
@@ -19,6 +19,57 @@ class Tetris {
         this.lastTime = 0;
 
         this.update();
+        this.keyControls();
+    }
+
+    collide(arena: Arena, player: Player) {
+        const [m, o] = [player.matrix, player.pos];
+        for (let y = 0; y < m.length; ++y) {
+            for(let x = 0; x < m[y].length; ++x) {
+                if(m[y][x] !== 0 &&
+                    (arena.matrix[y + o.y] && arena.matrix[y + o.y][x + o.x]) !== 0) {
+                return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    merge(arena: Arena, player: Player) {
+        player.matrix.forEach((row: number[], y: number) => {
+            row.forEach((value: number, x: number) => {
+                if(value !== 0) {
+                    arena.matrix[y + player.pos.y][x + player.pos.x] = value;
+                }
+            })
+        })
+    }
+
+    movePiece(dir: number) {
+        player.pos.x += dir;
+        if(this.collide(arena, player)) {
+            player.pos.x -= dir;
+        }
+    }
+
+    keyControls() {
+        document.addEventListener('keydown', event => {
+            if(event.keyCode === 65) {
+                this.movePiece(-1);
+            }
+            if(event.keyCode === 68) {
+                this.movePiece(1);
+            }
+            if(event.keyCode === 83) {
+                player.pos.y++;
+                if(this.collide(arena, player)) {
+                    player.pos.y--;
+                    this.merge(arena, player);
+                    player.pos.y = 0;
+                }
+                this.dropCounter = 0;
+            }
+        });
     }
 
     drawMatrix(matrix: number[][], offset: {x: number, y: number}) {
@@ -40,7 +91,8 @@ class Tetris {
     draw() {
         this.context.fillStyle = '#000';
         this.context.fillRect(0, 0, this.tetrisCanvas.width, this.tetrisCanvas.height);
-
+        
+        this.drawMatrix(arena.matrix, {x: 0, y: 0});
         this.drawMatrix(player.matrix, player.pos);
     }
 
@@ -51,6 +103,11 @@ class Tetris {
         this.dropCounter += deltaTime;
         if(this.dropCounter > this.dropInterval) {
             player.pos.y++;
+            if(this.collide(arena, player)) {
+                player.pos.y--;
+                this.merge(arena, player);
+                player.pos.y = 0;
+            }
             this.dropCounter = 0;
         }
 
@@ -61,8 +118,8 @@ class Tetris {
 
 //Tetris Pieces class
 class TetrisPieces {
-    pieceT: number[][];
-    constructor() {
+    public pieceT: number[][];
+    public constructor() {
         this.pieceT = [
             [0, 0, 0],
             [1, 1, 1],
@@ -77,15 +134,33 @@ class TetrisPieces {
 
 //Player class
 class Player {
-    matrix: number[][];
-    pos: {x: number, y: number};
+    public matrix: number[][];
+    public pos: {x: number, y: number};
 
-    constructor() {
+    public constructor() {
         this.matrix = tetrisPieces.getPiece();
         this.pos = {x: 5, y: 5};
     }
 }
 
+//Arena class
+class Arena {
+    public matrix: number[][];
+
+    public constructor() {
+        this.matrix = this.createMatrix(12, 20);
+    }
+
+    createMatrix(w: number, h: number) {
+        const matrix: number[][] = [];
+        while(h--) {
+            matrix.push(new Array(w).fill(0));
+        }
+        return matrix;
+    }
+}
+
 const tetrisPieces = new TetrisPieces;
 const player = new Player;
+const arena = new Arena;
 new Tetris;
