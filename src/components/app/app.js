1f91a1c112e9c7ec99143285605e40128cac96f9
Boriss Arhipov <bori.7166455@gmail.com>
2020-06-30 13:53:04 +0300

    Adding a player class and an update method

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,16 +6,36 @@ class Tetris {
         this.tetrisCanvas = document.getElementById('tetris');
         this.context = this.tetrisCanvas.getContext('2d');
         this.context.scale(20, 20);
-        this.context.fillStyle = '#000';
-        this.context.fillRect(0, 0, this.tetrisCanvas.width, this.tetrisCanvas.height);
-        tetrisPieces.getPiece().forEach((row, y) => {
+        this.dropCounter = 0;
+        this.dropInterval = 1000;
+        this.lastTime = 0;
+        this.update();
+    }
+    drawMatrix(matrix, offset) {
+        matrix.forEach((row, y) => {
             row.forEach((value, x) => {
                 if (value !== 0) {
                     this.context.fillStyle = 'red';
-                    this.context.fillRect(x, y, 1, 1);
+                    this.context.fillRect(x + offset.x, y + offset.y, 1, 1);
                 }
             });
         });
+    }
+    draw() {
+        this.context.fillStyle = '#000';
+        this.context.fillRect(0, 0, this.tetrisCanvas.width, this.tetrisCanvas.height);
+        this.drawMatrix(player.matrix, player.pos);
+    }
+    update(time = 0) {
+        const deltaTime = time - this.lastTime;
+        this.lastTime = time;
+        this.dropCounter += deltaTime;
+        if (this.dropCounter > this.dropInterval) {
+            player.pos.y++;
+            this.dropCounter = 0;
+        }
+        this.draw();
+        window.requestAnimationFrame(this.update.bind(this));
     }
 }
 class TetrisPieces {
@@ -30,5 +50,12 @@ class TetrisPieces {
         return this.pieceT;
     }
 }
+class Player {
+    constructor() {
+        this.matrix = tetrisPieces.getPiece();
+        this.pos = { x: 5, y: 5 };
+    }
+}
 const tetrisPieces = new TetrisPieces;
+const player = new Player;
 new Tetris;
--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -4,6 +4,9 @@ import './app.css';
 class Tetris {
     tetrisCanvas: HTMLCanvasElement;
     context: CanvasRenderingContext2D;
+    lastTime: number;
+    dropCounter: number;
+    dropInterval: number;
 
     constructor() {
         this.tetrisCanvas = document.getElementById('tetris') as HTMLCanvasElement;
@@ -11,19 +14,49 @@ class Tetris {
 
         this.context.scale(20, 20);
 
-        this.context.fillStyle = '#000';
-        this.context.fillRect(0, 0, this.tetrisCanvas.width, this.tetrisCanvas.height);
+        this.dropCounter = 0;
+        this.dropInterval = 1000;
+        this.lastTime = 0;
 
-        tetrisPieces.getPiece().forEach((row: number[], y: number) => {
+        this.update();
+    }
+
+    drawMatrix(matrix: number[][], offset: {x: number, y: number}) {
+        matrix.forEach((row: number[], y: number) => {
             row.forEach((value: number, x: number) => {
                 if(value !== 0) {
                     this.context.fillStyle = 'red';
-                    this.context.fillRect(x, y, 1, 1);
+                    this.context.fillRect(
+                        x + offset.x,
+                        y + offset.y,
+                        1,
+                        1
+                    );
                 }
             })
         })
     }
 
+    draw() {
+        this.context.fillStyle = '#000';
+        this.context.fillRect(0, 0, this.tetrisCanvas.width, this.tetrisCanvas.height);
+
+        this.drawMatrix(player.matrix, player.pos);
+    }
+
+    update(time = 0) {
+        const deltaTime: number = time - this.lastTime;
+        this.lastTime = time;
+        
+        this.dropCounter += deltaTime;
+        if(this.dropCounter > this.dropInterval) {
+            player.pos.y++;
+            this.dropCounter = 0;
+        }
+
+        this.draw();
+        window.requestAnimationFrame(this.update.bind(this));
+    }
 }
 
 //Tetris Pieces class
@@ -42,5 +75,17 @@ class TetrisPieces {
     }
 }
 
+//Player class
+class Player {
+    matrix: number[][];
+    pos: {x: number, y: number};
+
+    constructor() {
+        this.matrix = tetrisPieces.getPiece();
+        this.pos = {x: 5, y: 5};
+    }
+}
+
 const tetrisPieces = new TetrisPieces;
+const player = new Player;
 new Tetris;
